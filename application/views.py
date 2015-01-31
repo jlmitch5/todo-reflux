d12fb26c59232f4fb937732504f5005e50c8d7d5
Henrik Andersson <henrik@billogram.com>
2015-01-31 03:41:53 +0100

    Removed catch all route, implemented todos route

--- application/views.py
+++ application/views.py
@@ -1,7 +1,31 @@
 from application import app
-from flask import render_template
+from flask import render_template, jsonify
 
 @app.route('/', defaults={'path': ''})
-@app.route('/<path:path>')
 def index(path):
     return render_template('index.html')
+
+@app.route('/todos/')
+def todos():
+    return jsonify(todos=[
+            {
+                'title': "Discuss report with John",
+                'isChecked': False,
+                'key': 1
+            },
+            {
+                'title': "Get a haircut",
+                'isChecked': True,
+                'key': 2
+            },
+            {
+                'title': "Pay electricity bill",
+                'isChecked': True,
+                'key': 3
+            },
+            {
+                'title': "Check gym hours",
+                'isChecked': False,
+                'key': 4
+            }
+        ])
