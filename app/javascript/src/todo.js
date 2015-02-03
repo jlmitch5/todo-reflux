62a3b11827ff45d568c7022022d2cc03d3468d92
Henrik Andersson <henrik@billogram.com>
2015-02-03 07:39:02 +0100

    Documented Todo model

--- app/javascript/src/todo.js
+++ app/javascript/src/todo.js
@@ -1,5 +1,13 @@
 'use strict';
 
+/**
+ * Basic Todo model.
+ *
+ * @key Number Internal id
+ * @title String Represents what to do
+ * @isChecked Determines if a Todo is completed
+ * @createdAt When the Todo was created.
+ */
 var Todo = function (key, title, isChecked, createdAt) {
     this.key = key;
     this.title = title;
