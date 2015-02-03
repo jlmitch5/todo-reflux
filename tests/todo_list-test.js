b337f28a7409f62d98ca40ceb96696cff9178d9a
Henrik Andersson <henrik@billogram.com>
2015-02-03 01:36:19 +0100

    TodoListTest cleaned up

--- tests/todo_list-test.js
+++ tests/todo_list-test.js
@@ -14,27 +14,29 @@ describe('TodoList', function () {
     
     var Todo = require('../app/javascript/src/todo.js');
     var TodoItem = require('../app/javascript/src/todo_item.jsx');
+
     var TodoActions = require('../app/javascript/src/todo_actions.js');
     TodoActions.completeTodo.preEmit = function () {};
     TodoActions.removeTodo.preEmit = function () {};
 
     var TodoList = require('../app/javascript/src/todo_list.jsx');
 
-
     describe('renders', function () {
+        var container;
+        var todods;
+        var ul;
         
         it('an empty container when there is no todos', function () {
-            var container = TestUtils.renderIntoDocument(<TodoList todos={[]}/>);
+            container = TestUtils.renderIntoDocument(<TodoList todos={[]}/>);
 
             expect(container.getDOMNode().textContent).not.toBe('');
-
         });
 
         it('a todo list with 1 item', function () {
-            var todos = [new Todo(1, "Title", false, new Date())]; 
-            var container = TestUtils.renderIntoDocument(<TodoList todos={todos} />);
+            todos = [new Todo(1, "Title", false, new Date())]; 
+            container = TestUtils.renderIntoDocument(<TodoList todos={todos} />);
 
-            var ul = TestUtils.findRenderedDOMComponentWithTag(container, 'ul');
+            ul = TestUtils.findRenderedDOMComponentWithTag(container, 'ul');
 
             expect(ul).not.toBe(null);
             expect(ul.getDOMNode().children.length).toBe(1);
